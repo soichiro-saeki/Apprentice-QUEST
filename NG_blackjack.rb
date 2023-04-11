6b4e66a89f34bf42f8f84f028d4aba73cf86ab7a
github user name <goku.sou19941126@gmail.com>
2023-04-11 16:33:52 +0900

    new file:   NG_blackjack.rb 	modified:   blackjack.rb 	modified:   blackjack_deck.rb

---
+++ NG_blackjack.rb
@@ -0,0 +1,103 @@
+#ゲームの実行（勝敗の確認）
+# class BlackJack
+#   def judge(player, dealer)
+#     if player.score > dealer.score
+#       puts "あなたの勝ちです！"
+#     elsif player.score < dealer.score
+#       puts "ディーラーの勝ちです。"
+#     else
+#       puts "引き分けです。"
+#     end
+#   end
+
+
+class Card
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def value
+    case rank
+    when :A
+      1
+    when 2,3,4,5,6,7,8,9,10
+      rank
+    when :J, :Q, :K
+      10
+    end
+  end
+end
+
+class Deck
+  RANKS = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+  SUITS = [:"スペード", :"ダイヤ", :"ハート", :"クローバー"]
+
+  def initialize
+    @cards = RANKS.product(SUITS).map { |rank, suit| Card.new(rank, suit) }
+  end
+
+  def draw(n)
+    @cards.sample(n)
+  end
+end
+
+deck = Deck.new
+cards = deck.draw(2)
+total = cards.sum{|card| card.value}
+
+class Player
+  attr_reader :name, :cards
+
+  def initialize(name)
+    @name = name
+    @cards = []
+  end
+
+  def draw(deck)
+    @cards += deck.draw(2)
+  end
+
+  def total
+    @cards.sum(&:value)
+  end
+
+
+  def show_cards
+    puts "#{name}'s Cards: #{cards.map { |card| "#{card.rank} of #{card.suit}" }.join(', ')}"
+    puts "#{name}'s Total: #{total}"
+  end
+
+  def hit_or_stand(deck)
+    loop do
+      break if total >= 21
+
+      print "#{name}, do you want to hit or stand? (h/s): "
+      answer = gets.chomp.downcase
+      if answer == 'h'
+        draw(deck)
+        show_cards
+      else
+        break
+      end
+    
+    end
+  end
+
+
+deck = Deck.new
+player1 = Player.new("Alice")
+player2 = Player.new("Bob")
+
+[player1, player2].each do |player|
+  player.draw(deck)
+  player.show_cards
+end
+
+[player1, player2].each do |player|
+  player.hit_or_stand(deck)
+end
+
+end
