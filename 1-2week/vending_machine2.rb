dbab361aacaf5dffb942e0a66026e0e471140f50
soichiro-saeki <goku.sou19941126@gmail.com>
2023-04-13 14:40:52 +0900

    最初に何のクラスを定義しているかの記載を行った"rubocop"で

--- 1-2week/vending_machine2.rb
+++ 1-2week/vending_machine2.rb
@@ -1,69 +1,61 @@
+#自動販売機クラス
 class VendingMachine
- def drink_list
-  puts "sider:100円"
-  puts "coke:150円"
- end
- def press_button(drink)
-  @drink = drink
-  case drink
-  when "coke"
-    price = 150
-  when "cider"
-    price = 100
-  end
+   def drink_list
+    puts 'sider:100円'
+    puts 'coke:150円'
+   end
+   def press_button(drink)
+     @drink = drink
+     case drink
+     when 'coke'
+       price = 150
+     when 'cider'
+       price = 100
+   end
 
   if @coin >= price
-   puts "#{@drink}を購入しますか？(はい/いいえ)"
-   answer = gets.chomp
-    if answer == "はい"
-      puts "どの商品を購入しますか？（sider/coke）"
-       selected_drink = gets.chomp
-        case selected_drink
-        when answer == "sider"
-          puts "sider"
-        when answer == "coke"
-          puts "お金がたりません"
-        end
-      @coin -= price
-      puts "#{@drink}"
-    else
-     "いいえ"
-    end
-  else
-    puts "お金がたりません"
+    puts "#{@drink}を購入しますか？(はい/いいえ)"
+    answer = gets.chomp
+      if answer == "はい"
+        puts "どの商品を購入しますか？（sider/coke）"
+        selected_drink = gets.chomp
+          case selected_drink
+          when answer == "sider"
+            puts "sider"
+          when answer == "coke"
+            puts "お金がたりません"
+          end
+        @coin -= price
+        puts "#{@drink}"
+      else
+      "いいえ"
+      end
+      else
+       puts "お金がたりません"
   end
 
   puts "残高#{@coin}"
- end
- def deposit_coin(coin)
-  if coin == 100
-   @coin += coin
-  else
-   "100円硬貨のみ受け付けます"
   end
-end
+  def deposit_coin(coin)
+    if coin == 100
+    @coin += coin
+    else
+    "100円硬貨のみ受け付けます"
+    end
+  end
 
- def initialize(manufacturer, coin)
-  @manufacturer = manufacturer
-  @coin = coin
- end
+  def initialize(manufacturer, coin)
+    @manufacturer = manufacturer
+    @coin = coin
+  end
 
- private
+  private
 
- def press_manufacturer_name
-  @manufacturer
- end
+  def press_manufacturer_name
+    @manufacturer
+  end
 end
 
 vending_machine = VendingMachine.new('サントリー', 100)
 
 puts vending_machine.drink_list
-
-
-
-
-#vending_machine.deposit_coin(150)
-#puts vending_machine.press_button
-
-# vending_machine.deposit_coin(100)
-# puts vending_machine.press_button
