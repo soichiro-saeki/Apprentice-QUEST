139c3e482cd9da314d8828e01e5f8c0b5ba970d2
soichiro-saeki <goku.sou19941126@gmail.com>
2023-04-13 14:28:41 +0900

    ドリンクを選んでから購入するメソッドを追加中selected_drink.getsで入力を受け付ける

--- 1-2week/vending_machine2.rb
+++ 1-2week/vending_machine2.rb
@@ -1,9 +1,10 @@
 class VendingMachine
  def drink_list
   puts "sider:100円"
-  puts "coke150円"
+  puts "coke:150円"
  end
  def press_button(drink)
+  @drink = drink
   case drink
   when "coke"
     price = 150
@@ -12,17 +13,26 @@ class VendingMachine
   end
 
   if @coin >= price
-   puts "#{drink}を購入しますか？(はい/いいえ)"
+   puts "#{@drink}を購入しますか？(はい/いいえ)"
    answer = gets.chomp
     if answer == "はい"
-     @coin -= price
-     puts "#{drink}"
+      puts "どの商品を購入しますか？（sider/coke）"
+       selected_drink = gets.chomp
+        case selected_drink
+        when answer == "sider"
+          puts "sider"
+        when answer == "coke"
+          puts "お金がたりません"
+        end
+      @coin -= price
+      puts "#{@drink}"
     else
      "いいえ"
     end
   else
-    "お金がたりません"
+    puts "お金がたりません"
   end
+
   puts "残高#{@coin}"
  end
  def deposit_coin(coin)
@@ -45,10 +55,10 @@ end
  end
 end
 
-vending_machine = VendingMachine.new('サントリー', 200,)
+vending_machine = VendingMachine.new('サントリー', 100)
 
 puts vending_machine.drink_list
-puts vending_machine.press_button("cider")
+
 
 
 
