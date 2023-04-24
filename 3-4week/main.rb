6fa10b7ec9bb68e4e50749ffd7a575d74d266dbf
soichiro saeki <goku.sou19941126@gmail.com>
2023-04-24 12:19:00 +0900

    docker勉強中

--- 3-4week/main.rb
+++ 3-4week/main.rb
@@ -4,7 +4,6 @@ server = WEBrick::HTTPServer.new(
   DocumentRoot: './',
   BindAddress: '0.0.0.0',
   port: 8000
-
 )
 
 server.mount_proc('/') do |req, res|
@@ -12,4 +11,3 @@ server.mount_proc('/') do |req, res|
 end
 
 server.start
-
