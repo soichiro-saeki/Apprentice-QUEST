df6e0179311ccbae41e9c2d762ce976f6f93bd9e
soichiro-saeki <67487714+soichiro-saeki@users.noreply.github.com>
2023-04-27 16:32:48 +0900

    Create password_manager.sh

---
+++ 3-4week/password_manager.sh
@@ -0,0 +1,27 @@
+while true; do
+        echo "パスワードマネージャーへようこそ！: "
+        read -p "次の選択肢から入力してください Add Password/Get Password/Exit: " answer
+        case $answer in
+                "Add Password")
+                        read -p "サービス名を入力してください: " service
+                        read -p "ユーザー名を入力してください: " user
+                        read -p "パスワードを入力してください: " pass
+                        passfile=$(gpg -d passmanege.txt.gpg)
+                        echo $passfile$'\'$service:$user:$pass | gpg -e -r goku.sou19941126@gmail.com passmanege.txt
+                        ;;
+                "Get Password")
+                         gpg -d passmanege.txt.gpg > passmanege.txt
+                        read -p "検索するサービス名を入力してください: " search_service
+                        result=$(grep "^$search_service:" passmanege.txt)
+                        if [ -z "$result" ]; then
+                                echo "そのサービス名は登録されてません"
+                        else
+                                echo "$result"
+                        fi
+                        ;;
+                "Exit")
+                        echo Thank you!
+                        break
+                        ;;
+        esac
+done
